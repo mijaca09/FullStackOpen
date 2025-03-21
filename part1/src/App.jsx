7ffcc07d52d23776ff442b9a10444f2b75209b36
miguel-jaramillo <mijaca09@gmail.com>
2025-03-21 10:49:52 -0500

    en este punto se realizo el ejercicio 1.4 sin ningun problema

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,28 +4,30 @@ import Total from "./Total"
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+     exercises: 14
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-   exercises: 14
-}
+  ]
 
   return (
     <div>
       <Header course={course} />
       
-      <Content part={part1.name} exercises={part1.exercises} />
-      <Content part={part2.name} exercises={part2.exercises} />
-      <Content part={part3.name} exercises={part3.exercises} />
+      <Content part={parts[0].name} exercises={parts[0].exercises} />
+      <Content part={parts[1].name} exercises={parts[1].exercises} />
+      <Content part={parts[2].name} exercises={parts[2].exercises} />
 
-      <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
+      <Total exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </div>
   )
 }
