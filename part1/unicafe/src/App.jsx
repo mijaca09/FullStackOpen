bba40c3cc209e424f7ae7b296e2b3c85cbc1a76a
miguel-jaramillo <mijaca09@gmail.com>
2025-03-23 10:43:50 -0500

    aca se realiza el ejercicio 1.7 no entendi exactamente que se supone que tenia que hacer el average en el codigo, sin hembargo me da el mismo resultado que aparece en el ejemplo

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,11 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  // Calculamos estos valores directamente en el render
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all * 100
+
   return (
     <div>
       <h1>tiltle</h1>
@@ -16,6 +21,10 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   )
 }
